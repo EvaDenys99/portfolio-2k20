be77b1f68eb7e6a774c72c29147215f9bb57992f
Eva Denys <eva.denys@student.howest.be>
2020-10-19 20:12:30 +0200

    new bblogpost

--- components/card.js
+++ components/card.js
@@ -24,6 +24,7 @@ const Card = ({ article }) => {
     >
       <a className="col mb-4">
         <div>
+          {console.log("pdf")}
           <div className="mb-2">
             <img
               className={
